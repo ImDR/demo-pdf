f5e4ae15fee848e10fcbe079627e97b43aad2472
imdr <dineshrawataias@gmail.com>
2021-01-09 06:42:17 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
-function App() {
+import './App.css';
+import { useState } from 'react';
+
+const urls = [
+  '/pdf/pdf-test.pdf',
+  '/pdf/c4611_sample_explain.pdf',
+  '/pdf/sample.pdf',
+  '/pdf/pdf-sample.pdf',
+  '/pdf/gre_research_validity_data.pdf'
+];
+
+const App = () => {
+  const [i, setI] = useState(0);
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  const onNextClick = () => {
+    (i < urls.length - 1) && setI(i + 1);
+  };
+
+  const onPrevClick = () => {
+    (i > 0) && setI(i - 1);
+  };
+
+  console.log(i);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <button onClick={onPrevClick}>Prev</button>
+      <button onClick={onNextClick}>Next</button>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js">
+        <div
+          style={{
+            height: '100%',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Viewer
+            fileUrl={urls[i]}
+            plugins={[
+              defaultLayoutPluginInstance,
+            ]}
+          />
+        </div>
+      </Worker>
     </div>
   );
 }
